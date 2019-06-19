e077289d4d07fd710f42cd733d2287c063faa7db
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-06-19 16:14:20 +0700

    Add plus function.

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -1,7 +1,13 @@
-import { greets } from '../src/main';
+import { greets, plus } from '../src/main';
 
 describe('#greets', () => {
   it('greets the person', () => {
     expect(greets('Noptoon')).toBe('Hello, Noptoon')
+  });
+});
+
+describe('#plus', () => {
+  it('1+1 = 2', () => {
+    expect(plus(1, 1)).toBe(2)
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,2 @@
 export const greets = (name: string): string => `Hello, ${name}`
+export const plus = (parm1: number, parm2: number) => parm1 + parm2
