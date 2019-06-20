0d913b4f9ec4e07386d1e29b1539ef0187e80674
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-06-20 08:49:41 +0700

    Move eval logic into each op object

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -1,25 +1,52 @@
-import { calculator } from '../src/main';
+import { plus, minus, multiply } from '../src/main';
 
 describe('Calculator', () => {
-  describe('#plus', () => {
-    it('1+1 = 2', () => {
-      const plus = new calculator(1, 1)
-      expect(plus.displayExpression('+')).toBe('1 + 1 = 2')
-      expect(plus.result).toBe(2)
+  describe('computations', () => {
+    describe('#plus', () => {
+      describe('creates an object that represents computation which:', () => {
+        it('can evaluate results', () => {
+          expect(plus(1, 1).eval()).toBe(2)
+          expect(plus(10, 0).eval()).toBe(10)
+        });
+
+        it('contains left, right operands and operator symbol `+`', () => {
+          expect(plus(1, 1)).toMatchObject({ op: '+', left: 1, right: 1 })
+          expect(plus(10, 0)).toMatchObject({ op: '+', left: 10, right: 0 })
+        });
+      })
     });
-  });
-  describe('#multiply', () => {
-    it('2*3 = 6', () => {
-      const multiply = new calculator(2, 3)
-      expect(multiply.displayExpression('*')).toBe('2 * 3 = 6')
+
+    describe('#minus', () => {
+      describe('creates an object that represents computation which:', () => {
+        it('can evaluate results', () => {
+          expect(minus(1, 10).eval()).toBe(-9)
+          expect(minus(10, 0).eval()).toBe(10)
+        });
+
+        it('contains left, right operands and operator symbol `-`', () => {
+          expect(minus(1, 10)).toMatchObject({ op: '-', left: 1, right: 10 })
+          expect(minus(10, 0)).toMatchObject({ op: '-', left: 10, right: 0 })
+        });
+      })
     });
-  });
-  describe('#minus', () => {
-    it('3-2 = 1', () => {
-      const minus = new calculator(3, 2)
-      expect(minus.displayExpression('-')).toBe('3 - 2 = 1')
+
+    describe('#multiply', () => {
+      describe('creates an object that represents computation which:', () => {
+        it('can evaluate results', () => {
+          expect(multiply(1, 10).eval()).toBe(10)
+          expect(multiply(10, 0).eval()).toBe(0)
+          expect(multiply(7, 5).eval()).toBe(35)
+        });
+
+        it('contains left, right operands and operator symbol `×`', () => {
+          expect(multiply(1, 10)).toMatchObject({ op: '×', left: 1, right: 10 })
+          expect(multiply(10, 0)).toMatchObject({ op: '×', left: 10, right: 0 })
+          expect(multiply(7, 5)).toMatchObject({ op: '×', left: 7, right: 5 })
+        });
+      })
     });
-  });
+
+  })
 });
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,70 +1,19 @@
-export class minus {
-  param1: number
-  param2: number
+export const plus = (left: number, right: number) => ({
+  op: '+',
+  left, right,
+  eval: () => left + right
+})
 
-  constructor(param1: number, param2: number) {
-    this.param1 = param1
-    this.param2 = param2
-  }
 
-  displayExpression() {
-    return `${this.param1} - ${this.param2} = ${this.param1 - this.param2}`
-  }
-}
+export const minus = (left: number, right: number) => ({
+  op: '-',
+  left, right,
+  eval: () => left - right
+})
 
-export class multiply {
-  param1: number
-  param2: number
 
-  constructor(param1: number, param2: number) {
-    this.param1 = param1
-    this.param2 = param2
-  }
-
-  displayExpression() {
-    return `${this.param1} x ${this.param2} = ${this.param1 * this.param2}`
-  }
-}
-
-export class plus {
-  param1: number
-  param2: number
-
-  constructor(param1: number, param2: number) {
-    this.param1 = param1
-    this.param2 = param2
-  }
-
-  displayExpression() {
-    return `${this.param1} + ${this.param2} = ${this.param1 + this.param2}`
-  }
-}
-
-export class calculator {
-  param1: number
-  param2: number
-  result: number
-
-  constructor(param1: number, param2: number) {
-    this.param1 = param1
-    this.param2 = param2
-  }
-
-  displayExpression(operator: string) {
-    switch (operator) {
-      case '+':
-        this.result = this.param1 + this.param2
-        return `${this.param1} ${operator} ${this.param2} = ${this.param1 + this.param2}`
-      case '-':
-        this.result = this.param1 - this.param2
-        return `${this.param1} ${operator} ${this.param2} = ${this.param1 - this.param2}`
-      case '*':
-        this.result = this.param1 * this.param2
-        return `${this.param1} ${operator} ${this.param2} = ${this.param1 * this.param2}`
-      default:
-        return ''
-    }
-
-  }
-
-}
+export const multiply = (left: number, right: number) => ({
+  op: '×',
+  left, right,
+  eval: () => left * right
+})
